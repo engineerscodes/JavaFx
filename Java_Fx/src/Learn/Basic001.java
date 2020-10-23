abcdf9ed23fc71853d5c07fb4157420824f1f44d
engineerscodes <practicalengineers05@gmail.com>
2020-10-23 22:01:07 +0530

    Initial Commit

--- Java_Fx/src/Learn/Basic001.java
+++ Java_Fx/src/Learn/Basic001.java
@@ -11,6 +11,7 @@ import javafx.scene.*;
  *  RANDOM CODER'S
  *  Tech/Project Lead Android Club
  */
+import javafx.application.Platform;
 public class Basic001  extends Application
 {
    public void init()
@@ -23,13 +24,16 @@ public class Basic001  extends Application
    }
 	@Override
 	public void start(Stage primaryStage) throws Exception 
-	{
+	{   //javafx.application.Platform.exit();
+		//Platform x=new Platform();
 		Scene new_scene=new Scene(new Group(),500,500);
 		primaryStage.setTitle("BASIC_01");
 		primaryStage.setScene(new_scene);
 		primaryStage.show();
+
 		/*Note that you don't need the main() method to run JavaFX. For example, this code can be
 compiled and run from the command line:*/
+	//	javafx.application.Platform.setImplicitExit(true);
 	}
 	public void stop()
 	{
---
+++ Java_Fx/src/Learn/Basic002.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package Learn;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.*;
+import javafx.stage.Stage;
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class Basic002 extends Application{
+
+	/**
+	 *  closing Window using Platform.exit();
+	 */
+	public static void main(String[] args)
+	{
+       launch(args);
+	}
+public void start()
+{
+	System.out.println("Before");
+}
+public void stop()
+{
+	System.out.println("After");
+}
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+         // Platform.exit();
+          Scene root=new Scene(new Group(),500,500);
+          primaryStage.setTitle("closing Window using Platform.exit()");
+          primaryStage.setScene(root);
+          primaryStage.show();
+          Platform.setImplicitExit(false);//to stop from exiting 
+	}
+
+}
+
