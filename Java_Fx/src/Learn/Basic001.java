a31764b5af2098c1e1401b0f4b3c252731d0692c
engineerscodes <practicalengineers05@gmail.com>
2020-10-26 22:46:12 +0530

    new file

--- Java_Fx/src/Learn/Basic001.java
+++ Java_Fx/src/Learn/Basic001.java
@@ -6,6 +6,7 @@ import java.util.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.*;
+import javafx.scene.paint.Color;
 /**
  *  @author M.NAVEEN
  *  RANDOM CODER'S
@@ -22,19 +23,22 @@ public class Basic001  extends Application
    {
 	   launch(strings);
    }
-	@Override
-	public void start(Stage primaryStage) throws Exception 
+   
+@Override
+	 public void start(Stage primaryStage) throws Exception 
 	{   //javafx.application.Platform.exit();
 		//Platform x=new Platform();
 		Scene new_scene=new Scene(new Group(),500,500);
+		new_scene.setFill(Color.DARKSLATEGREY);
 		primaryStage.setTitle("BASIC_01");
 		primaryStage.setScene(new_scene);
 		primaryStage.show();
-
+	}
 		/*Note that you don't need the main() method to run JavaFX. For example, this code can be
 compiled and run from the command line:*/
-	//	javafx.application.Platform.setImplicitExit(true);
-	}
+//	//	javafx.application.Platform.setImplicitExit(true);
+//	} 
+        
 	public void stop()
 	{
 		System.out.println("After");
