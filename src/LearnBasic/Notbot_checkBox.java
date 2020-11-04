93e0e2d6c07a8da5c46dff5399627b8eb2bb437b
engineerscodes <practicalengineers05@gmail.com>
2020-11-04 20:01:03 +0530

    Initial Commit

--- src/LearnBasic/Notbot_checkBox.java
+++ src/LearnBasic/Notbot_checkBox.java
@@ -1,6 +1,7 @@
 package LearnBasic;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.HBox;
@@ -20,13 +21,14 @@ public class Notbot_checkBox extends Application
         Rectangle rc=new Rectangle();
         rc.setWidth(400);
         rc.setHeight(400);
-
+        Group group=new Group();
         rc.setFill(Color.RED);
-        StackPane sp=new StackPane();
-        sp.getChildren().add(rc);
-        sp.getChildren().add(cb);
-
-        Scene root=new Scene(sp,500,500);
+        //StackPane sp=new StackPane();
+       // sp.getChildren().add(rc);
+       // sp.getChildren().add(cb);
+         group.getChildren().add(rc);
+         group.getChildren().add(cb);
+        Scene root=new Scene(group,500,500);
         primaryStage.setScene(root);
         primaryStage.setTitle("NOT A BOT");
         primaryStage.show();
