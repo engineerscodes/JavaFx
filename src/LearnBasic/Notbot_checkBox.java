4bd04cc3565b6c3299289dcad1d0c5dcb2140680
engineerscodes <practicalengineers05@gmail.com>
2020-11-04 19:21:15 +0530

    Initial Commit

--- src/LearnBasic/Notbot_checkBox.java
+++ src/LearnBasic/Notbot_checkBox.java
@@ -13,11 +13,12 @@ public class Notbot_checkBox extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        CheckBox cb=new CheckBox("clickme");
+        CheckBox cb=new CheckBox("I'm not a robot");
         StackPane sp=new StackPane();
         sp.getChildren().add(cb);
         Scene root=new Scene(sp,500,500);
         primaryStage.setScene(root);
+        primaryStage.setTitle("NOT A BOT");
         primaryStage.show();
     }
 }
