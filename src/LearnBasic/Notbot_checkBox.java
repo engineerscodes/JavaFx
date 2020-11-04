c61baf5329fef2647a1b5448f70c6328d6db2276
engineerscodes <practicalengineers05@gmail.com>
2020-11-04 19:32:02 +0530

    Initial Commit

--- src/LearnBasic/Notbot_checkBox.java
+++ src/LearnBasic/Notbot_checkBox.java
@@ -5,6 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Notbot_checkBox extends Application
@@ -14,8 +17,15 @@ public class Notbot_checkBox extends Application
     public void start(Stage primaryStage) throws Exception
     {
         CheckBox cb=new CheckBox("I'm not a robot");
+        Rectangle rc=new Rectangle();
+        rc.setWidth(400);
+        rc.setHeight(400);
+
+        rc.setFill(Color.RED);
         StackPane sp=new StackPane();
+        sp.getChildren().add(rc);
         sp.getChildren().add(cb);
+
         Scene root=new Scene(sp,500,500);
         primaryStage.setScene(root);
         primaryStage.setTitle("NOT A BOT");
