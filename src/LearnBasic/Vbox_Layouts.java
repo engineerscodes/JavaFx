9b8f710dbefeb3530cd6960ead24cd27d59eb0e1
engineerscodes <practicalengineers05@gmail.com>
2020-11-04 22:01:32 +0530

    Initial Commit

---
+++ src/LearnBasic/Vbox_Layouts.java
@@ -0,0 +1,42 @@
+package LearnBasic;
+import java.util.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Vbox_Layouts  extends Application
+{
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        VBox v=new VBox();
+        v.getChildren().addAll(new Circle(40, Color.MAGENTA),new Rectangle(100,200,Color.YELLOW));
+        primaryStage.setScene(new Scene(v));
+        primaryStage.show();
+    }
+
+    public static void main(String ...arg)
+    {
+        launch(arg);
+    }
+}
