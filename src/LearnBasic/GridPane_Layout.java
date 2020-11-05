19f03d3eeb5d80dda571cc696937962b2033912c
engineerscodes <practicalengineers05@gmail.com>
2020-11-05 15:28:00 +0530

    Initial Commit

---
+++ src/LearnBasic/GridPane_Layout.java
@@ -0,0 +1,36 @@
+package LearnBasic;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GridPane_Layout extends Application
+{    Stage window;
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    public void start(Stage primaryStage) throws Exception
+    {
+            window=primaryStage;
+            GridPane gp=new GridPane();
+            gp.setPadding(new Insets(10,10,10,10));
+            window.setScene(new Scene(gp));
+            window.setTitle("GRID PANE -");
+            window.show();
+
+    }
+}
